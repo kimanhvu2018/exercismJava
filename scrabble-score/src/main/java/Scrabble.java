9a73eb8b78b9e84e9f151dab6cec177a396de0ce
kimanhvu2018 <kim-anhv@hotmail.com>
2018-08-21 03:41:11 +0100

    starting scrabble exercise

---
+++ scrabble-score/src/main/java/Scrabble.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+class Scrabble {
+
+    Scrabble(String word) {
+    HashMap<Character, Integer> letterVals = new HashMap<Character, Integer>();
+
+    letterVals.put('A', 1);
+    letterVals.put('B', 3);
+    letterVals.put('C', 3);
+    letterVals.put('D', 2);
+    letterVals.put('E', 1);
+    letterVals.put('F', 4);
+    letterVals.put('G', 2);
+    letterVals.put('H', 4);
+    letterVals.put('I', 1);
+    letterVals.put('J', 8);
+    letterVals.put('K', 5);
+    letterVals.put('L', 3);
+    letterVals.put('M', 3);
+    letterVals.put('N', 1);
+    letterVals.put('O', 1);
+    letterVals.put('P', 3);
+    letterVals.put('Q', 10);
+    letterVals.put('R', 1);
+    letterVals.put('S', 1);
+    letterVals.put('T', 1);
+    letterVals.put('U', 1);
+    letterVals.put('V', 4);
+    letterVals.put('W', 4);
+    letterVals.put('X', 8);
+    letterVals.put('Y', 4);
+    letterVals.put('Z', 10);
+
+    word = word.toUpperCase();
+    char[] lettersword = word.toCharArray();
+
+
+        int getScore () {
+
+        }
+    }
+}
